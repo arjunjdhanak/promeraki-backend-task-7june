512716241c5a4e0ff6de0ae6bc3da38e816d35f4
arjunjdhanak <arjunjdhanak@gmail.com>
2025-06-07 14:05:14 +0000

    added local db

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PartsModule } from './parts/parts.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [PartsModule],
+  imports: [PartsModule, MongooseModule.forRoot('mongodb://localhost/parts-inventory'),],
   controllers: [AppController],
   providers: [AppService],
 })
